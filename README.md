c3cb74d5a6b2618f338d9cc7bae0d0d67af9216c
Marcel Schmager <schmager29@gmail.com>
2020-06-10 13:39:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a very small projct which uses the youtube api :)
+
+You can find a preview here [http://youtube.marcel-schmager.com/](http://youtube.marcel-schmager.com/)
---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { Grid } from '@material-ui/core';
+
+import { SearchBar, VideoList, VideoDetail } from './components';
+
+import youtube from './api/youtube';
+
+console.log(process.env.REACT_APP_API_KEY);
+class App extends React.Component {
+	state = {
+		videos: [],
+		selectedVideo: null
+	};
+
+	componentDidMount() {
+		this.handleSubmit('Javascript');
+	}
+
+	handleSubmit = async (searchTerm) => {
+		const response = await youtube.get('search', {
+			params: {
+				part: 'snippet',
+				maxResults: 5,
+				key: process.env.REACT_APP_API_KEY,
+				q: searchTerm
+			}
+		});
+
+		this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+	};
+
+	onVideoSelect = (video) => {
+		this.setState({ selectedVideo: video });
+	};
+
+	render() {
+		const { selectedVideo, videos } = this.state;
+		return (
+			<Grid justify="center" container spacing={10}>
+				<Grid item xs={12}>
+					<Grid container spacing={10}>
+						<Grid item xs={12}>
+							<SearchBar onFormSubmit={this.handleSubmit} />
+						</Grid>
+						<Grid item xs={8}>
+							<VideoDetail video={selectedVideo} />
+						</Grid>
+						<Grid item xs={4}>
+							<VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		);
+	}
+}
+
+export default App;
